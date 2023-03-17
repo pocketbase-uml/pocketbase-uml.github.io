53b0e1c0fbade196ecf74efcb41fe875f191962b
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-03-17 18:30:13 +0200

    Remove analytics & update deps

--- src/lib/analytics.ts
+++
@@ -1,8 +0,0 @@
-import { dev } from '$app/environment';
-import mixpanel from 'mixpanel-browser';
-import { MIXPANEL_PROJECT_ID } from './config';
-mixpanel.init(MIXPANEL_PROJECT_ID, { debug: dev });
-
-export function log(path: string) {
-  mixpanel.track(path);
-}
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,5 +11,3 @@ export const MAX_ZOOM = 2;
 export const MAX_VISIBLE_NOTIFICATIONS = 5;
 export const NOTIFICATION_TIMEOUT = 3000;
 export const PUBLIC_URL = 'https://pocketbase-uml.github.io';
-
-export const MIXPANEL_PROJECT_ID = '0a8b2e5d21c92760ab71c450b24ea89e';
