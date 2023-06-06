a660a94e4454fea25d5c5ee45c0397932cff22c3
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-06-06 20:51:40 +0300

    Update deps, improve testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,38 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry'
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    }
+  ],
+
   webServer: {
     command: 'npm run build && npm run preview',
     port: 4173
-  },
-  testDir: 'tests'
-};
-
-export default config;
+  }
+});
